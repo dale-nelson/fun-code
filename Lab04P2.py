701ae0b15bf35d7c7a54a94083e8357ef1d20bb6
dale-nelson <codingdev.dale@gmail.com>
2019-11-12 17:50:48 -0700

    Factorial code for Lab4

---
+++ Lab04P2.py
@@ -0,0 +1,7 @@
+def factorial(i):
+  if i <= 1:
+    return i
+  if factorial:
+    return i*factorial(i-1)
+
+print(factorial(4))
--- recursion.py
+++ recursion.py
@@ -1,11 +1,3 @@
-def factorial(i):
-  if i <= 1:
-    return i
-  if factorial:
-    return i*factorial(i-1)
-
-print(factorial(4))
-
 def naturalNumberAddition(i):
   if i <= 1:
     return i
