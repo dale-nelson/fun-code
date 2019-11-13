10d0f57fcbb2e0fb9ecbd6758311d2a13ab8100e
dale-nelson <codingdev.dale@gmail.com>
2019-11-12 20:04:17 -0700

    Name and code overhaul

--- Lab04P1.py
+++ Lab04P1.py
@@ -1,32 +1,33 @@
 def main():
-  print("This program calculates the nth value of the Fibonacci sequence.")
-  valid_check = isInputValid()
-  pos_check = isPos(valid_check)
-  print(fibonacci(pos_check))
-  return
+    print("This program will calculate the nth value of the Fibonacci sequence.")
+    userInput = validLoop()
+    posCheck = isPos(userInput)
+    print("The value of the fib sequence at position {0} is {1}".format(posCheck,fib(posCheck)))
+    return
 
-def fibonacci(i):
-  if i <= 1:
-    return i
-  #If i > 1, return the following:
-  return fibonacci(i - 1) + fibonacci(i - 2)
+def fib(i):
+    if i <= 1 or i is None:
+        return i
+    return fib(i - 1) + fib(i - 2)
 
-def userInput():
+def validCheck():
     try:
         return int(float(input()))
     except ValueError:
-        print("Your input is not a number. Please try again.")
+        print("That value is not a number. Please try again.")
 
-def isInputValid():
-    ui = userInput()
-    if not userInput():
-        ui = userInput()
-    return ui
+def validLoop():
+    inp = validCheck()
+    while inp is None:
+        inp = validCheck()
+    else:
+        return inp
 
 def isPos(i):
-    if i < 0:
-        print("Negative numbers are invalid for the Fibonacci sequence. Please try again.")
-        i = isInputValid()
-    return i
+    while i < 0:
+        print("Negative numbers cannot be accepted. Please try again.")
+        i = validLoop()
+    else:
+        return i
 
 main()
